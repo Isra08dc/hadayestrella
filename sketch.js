49690fc341585db71050f4ac2743a38df3d1135e
Isra08dc <90532141+Isra08dc@users.noreply.github.com>
2021-11-06 17:12:01 -0600

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,65 @@
+var starImg,bgImg;
+var star, starBody;
+//crea la variable para el sprite del hada y fairyImg
+
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+
+function preload()
+{
+	starImg = loadImage("images/star.png");
+	bgImg = loadImage("images/starNight.png");
+	
+function setup() {
+	createCanvas(800, 750);
+
+	fairyVoice = loadSound("JoyMusic.mp3");
+
+	fairyImg = loadAnimation("images/fairyImage1.png, images/fairyImage2.png");
+}
+
+
+	star = createSprite(650,30);
+	star.addImage(starImg);
+	star.scale = 0.2;
+
+    fairy = createSprite(200, 200);
+	fairy.addImage(fairyImg);
+	fairy.scale = 0.2;
+
+	engine = Engine.create();
+	world = engine.world;
+
+	starBody = Bodies.circle(650 , 30 , 5 , {restitution:0.5, isStatic:true});
+	World.add(world, starBody);
+	
+	Engine.run(engine);
+
+}
+
+
+function draw() {
+  background(bgImg);
+
+  star.x= starBody.position.x 
+  star.y= starBody.position.y 
+
+  console.log(star.y);
+
+  //escribe aquí el código para detener la estrella en la mano del hada
+
+  drawSprites();
+
+}
+
+function keyPressed() {
+
+	if (keyCode === DOWN_ARROW) {
+		Matter.Body.setStatic(starBody,false); 
+	}
+
+	//escribe el código para mover al hada a la izquierda y derecha
+	
+}
